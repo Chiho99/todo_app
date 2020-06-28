304af95b0e5aaea957bdcff49d9250cb32cc1872
Chiho99 <chihomikami.81@gmail.com>
2020-06-28 23:49:43 +0900

    write down select and image change function

--- store.php
+++ store.php
@@ -6,11 +6,12 @@ require_once('Models/Task.php');
 $title = $_POST['title'];
 $contents = $_POST['contents'];
 $deadline = $_POST['deadline'];
+$list = $_POST['list'];
 $currrentTime = date("Y/m/d H:i:s");
 
 // DBへのデータ保存
 $task = new Task();
-$task->create([$title, $contents, $currentTime, $deadline]);
+$task->create([$title, $contents, $currentTime, $deadline, $list]);
 
 
 // リダイレクト
